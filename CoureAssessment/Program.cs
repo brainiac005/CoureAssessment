2b62020f945075f0ebdf433882c3c395dbcf3986
Botosoft <Botosoft@HPDPS10>
2022-02-22 11:57:38 +0100

    Add project files.

---
+++ CoureAssessment/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CoureAssessment
+{
+    //program to give an array of integers that keeps a total score based on parameters
+   public class Program
+    {
+        //declaration of array
+        int[] myArr = new int[] {8,8}; 
+
+        int ArrayFunction()
+        {
+            int points = 0;
+            var arrayLength = myArr.Length;
+
+            //loop through each element in the array
+            for (int i = 0; i<arrayLength; i++)
+            {
+                //check for first paramter ** Add 1 for each even number in the array
+                var mod1 = myArr[i] % 2;
+                if (mod1 == 0)
+                {
+                    //element is an even number
+                    points += 1;
+                }
+
+                //check for second paramter ** Add 3 for odd even number in the array
+                var mod2 = myArr[i] % 2;
+                if (mod2 != 0)
+                {
+                    //element is an even number
+                    points += 3;
+                }
+
+                //check for third paramter ** Add 5 for every time you encounter an 8 in the array
+                
+                if (myArr[i] == 8)
+                {
+                    //element is an even number
+                    points += 5;
+                }
+            }
+
+            return points;
+        }
+        //get the total elements in the array
+        
+        static void Main(string[] args)
+        {
+            var myObj = new Program();
+            Console.WriteLine("Output: "+ myObj.ArrayFunction());
+        }
+    }
+}
